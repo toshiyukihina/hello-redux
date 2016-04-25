3022701793fa2a6cfd9e4ab4b512767165fecf6d
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-04-25 18:48:46 +0900

    Add main component

---
+++ app/assets/javascripts/main.js.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+class Main extends React.Component {
+
+  render() {
+    return (
+      <Provider>
+      </Provider>
+    );
+  }
+  
+}
+
+export default Main;
